745591238762991eae56e437ecea8d9e60f6f880
Devil666face <artem1999k@gmail.com>
2022-09-11 11:00:23 +0300

    Добавлено отображени фото в админке

--- personalsite/blog/admin.py
+++ personalsite/blog/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Article, Category
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title','created_at','update_at','category','published')
+    list_display = ('title','created_at','update_at','category','published', 'get_photo')
     list_display_links = ('title',)
     search_fields = ('title','content')
     list_editable = ('published','category')
     list_filter = ('published', 'category','created_at')
+    fields = ('title','photo','get_photo','content','created_at','update_at','category','published')
+    readonly_fields = ('get_photo','created_at','update_at')
+    # fieldsets есть такой метод для большей кастомизации
+    def get_photo(self, obj):
+        return mark_safe(f'<img src="{obj.photo.url}" width="150">') if obj.photo else '❌'
+
+    get_photo.short_description = 'Фото'
+            
+
 
 class Categorydmin(admin.ModelAdmin):
     # list_display = ('id','title')
--- personalsite/blog/models.py
+++ personalsite/blog/models.py
@@ -18,7 +18,7 @@ class Article(models.Model):
     content = models.TextField(blank=True, verbose_name='Контент')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
     update_at = models.DateTimeField(auto_now=True, verbose_name='Изменено')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True, verbose_name='Фото')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True, verbose_name='Текущее фото')
     file = models.FileField(upload_to='files/%Y/%m/%d/',blank=True, verbose_name='Прикрепленный файл')
     published = models.BooleanField(default=True, verbose_name='Опубликовано')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория', related_name='get_articles')
